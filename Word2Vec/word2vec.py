7810d0bc238cd6c4e5a5549c22843b17cde82815
Ryan Kim <rck2001@hotmail.com>
2023-11-22 15:13:58 -0500

    jupiter :earth_asia:

--- Word2Vec/word2vec.py
+++ Word2Vec/word2vec.py
@@ -1,7 +1,14 @@
+import pandas as pd
 import gensim.downloader as api
 
 # Task 1 - Evaluation of the word2vec-google-news-300 Pre-trained Model
+# Load the dataset
+df = pd.read_csv('synonym.csv')
+questions = df['question'].tolist()
+
+# Word2Vec gooogle news model
 wv = api.load('word2vec-google-news-300')
+
 
 # Task 2 - Comparison with other pre-trained models
 
