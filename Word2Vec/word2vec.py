7c79d9f13dc7757e5a70d4587e8583a2da37b297
Ryan Kim <rck2001@hotmail.com>
2023-11-22 12:27:47 -0500

    not working word2vec download

--- Word2Vec/word2vec.py
+++ Word2Vec/word2vec.py
@@ -1,6 +1,7 @@
-
+import gensim.downloader as api
 
 # Task 1 - Evaluation of the word2vec-google-news-300 Pre-trained Model
+wv = api.load('word2vec-google-news-300')
 
 # Task 2 - Comparison with other pre-trained models
 
